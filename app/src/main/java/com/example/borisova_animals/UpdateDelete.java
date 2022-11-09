cb25e250417f3e258189e5c7abf60c88f976b282
elenabor2003@gmail.com <elenabor2003@gmail.com>
2022-11-09 16:20:40 +0300

    commit 09.11.22

--- app/src/main/java/com/example/borisova_animals/UpdateDelete.java
+++ app/src/main/java/com/example/borisova_animals/UpdateDelete.java
@@ -10,5 +10,7 @@ public class UpdateDelete extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_update_delete);
+
+
     }
 }
